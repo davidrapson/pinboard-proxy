a07ca65ca200298c82281a642688bb9256a8c517
David Rapson <david@davidrapson.co.uk>
2015-03-08 21:44:13 +0000

    Use auth token from environment

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var requestPromise = require('./lib/requestPromise');
 
 var app = express();
 
+var AUTH_TOKEN = process.env.AUTH_TOKEN;
+
 app.set('port', (process.env.PORT || 8000));
 
 app.get('/', function (req, res) {
@@ -11,19 +13,18 @@ app.get('/', function (req, res) {
 });
 
 app.get('/liked.json',
-    apicache('60 seconds'),
+    apicache('60 minutes'),
     function (req, res) {
         requestPromise({
             hostname: 'api.pinboard.in',
-            path: '/v1/posts/all?auth_token=davidrapson:CA3E51156087CF794305&format=json&tag=liked&shared=yes'
+            path: '/v1/posts/all?auth_token=' + AUTH_TOKEN + '&format=json&tag=liked&shared=yes'
         }).then(function(data) {
             res.header('Access-Control-Allow-Origin', '*');
             res.set('Content-Type', 'application/json');
-            // res.set('Cache-Control', 'public, max-age=' + ms('5s'));
             res.send(data);
         });
     });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
